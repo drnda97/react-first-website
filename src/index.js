59ff7649a73dcce108b9cf4cb6f7d01416c618ec
drnda97 <drndica54@gmail.com>
2021-10-01 16:24:32 +0200

    checking build

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import React from 'react';
 import ReactDom from 'react-dom'
 
 function Greeting() {
-  return <h4>Hello World!</h4>
+  return <h4>Hello Again build is done!</h4>
 }
 ReactDom.render(<Greeting />, document.getElementById('root'));
