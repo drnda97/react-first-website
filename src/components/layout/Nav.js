0b1f3b060fcacc59f93e4c9280d6a2beff9390b6
drnda97 <drndica54@gmail.com>
2021-11-01 14:41:12 +0100

    fixed warrnings

--- src/components/layout/Nav.js
+++ src/components/layout/Nav.js
@@ -4,20 +4,20 @@ const Nav = () => {
     return (
         <nav className='w-100 d-flex'>
             <div className='w-50 text-center'>
-                <a href="#">
+                <a href="/">
                     <img src={logo} alt='logo' className="my-3 w-10"/>
                 </a>
             </div>
             <div className='ml-25 w-25'>
                 <ul className='d-flex w-100 mt-4'>
                     <li className="col-4 mt-4">
-                        <a href="#">Pocetna</a>
+                        <a href="/">Pocetna</a>
                     </li>
                     <li className="col-4 mt-4">
-                        <a href="#">Cenovnik</a>
+                        <a href="/">Cenovnik</a>
                     </li>
                     <li className="col-4 mt-4">
-                        <a href="#">Sklapanje</a>
+                        <a href="/">Sklapanje</a>
                     </li>
                 </ul>
             </div>
